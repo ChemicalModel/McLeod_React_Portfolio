08a65353a883f93eae4c3ec31f5c5f55e63fb0ec
ChemicalModel <michaelmcleod133@gmail.com>
2023-05-31 20:54:06 -0400

    After receiving some pointers, changed dependencies and refactored some code.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import AboutMe from "./pages/AboutMe";
+import Projects from "./components/Project";
+import Footer from "./components/Footer";
+import Resume from "./pages/Resume";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <main>
+      <h2>Contact</h2>
+      <p>Sorry Ms. Jackson OOOOO</p>
+    </main>
+  );
+};
+
+export default Contact;
